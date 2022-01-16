04e95a7371a78c3f0b6fd459a249b02052a1fcb1
sasugadayo <sasugaff@icloud.com>
2022-01-16 22:06:55 +0900

    second commit

---
+++ ecsite/resources/views/item/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-left">
+            @foreach ($items as $item)
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <!-- 以下の部分を修正 -->
+                    <div class="card-header">
+                        <a href="/item/{{ $item->id }}">{{ $item->name }}</a>
+                    </div>
+                    <div class="card-body">
+                        {{ $item->amount }}
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+        <div class="row justify-content-center">
+            {{ $items->appends(['keyword' => Request::get('keyword')])->links() }}
+        </div>
+    </div>
+@endsection
--- ecsite/routes/web.php
+++ ecsite/routes/web.php
@@ -11,10 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'ItemController@index');
+
+Route::get('/item/{item}', 'ItemController@show');
