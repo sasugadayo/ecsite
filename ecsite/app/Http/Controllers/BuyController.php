888e95b9044c0ed8046672aecaf3ddb6b43a0d8e
sasugadayo <sasugaff@icloud.com>
2022-01-17 21:47:55 +0900

    third commit

---
+++ ecsite/app/Http/Controllers/BuyController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CartItem; //追加
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth; //追加
+use App\Mail\Buy;
+use Illuminate\Support\Facades\Mail;
+
+class BuyController extends Controller {
+    public function index()     {
+        $cartitems = CartItem::select('cart_items.*', 'items.name', 'items.amount')
+        ->where('user_id', Auth::id())
+        ->join('items', 'items.id','=','cart_items.item_id')
+        ->get();
+        $subtotal = 0;
+        foreach($cartitems as $cartitem){
+            $subtotal += $cartitem->amount * $cartitem->quantity;
+        }
+        return view('buy/index', ['cartitems' => $cartitems, 'subtotal' => $subtotal]);
+    }
+    // public function store(Request $request)
+    // {
+    //     if( $request->has('post') ){
+    //         CartItem::where('user_id', Auth::id())->delete();
+    //         return view('buy/complete');
+    //     }
+    //     $request->flash();
+    //     return $this->index();
+    // }
+    public function store(Request $request)
+    {
+        if( $request->has('post') ){
+            // MailクラスとBuyクラスを使ってメールを送信する
+            Mail::to(Auth::user()->email)->send(new Buy());
+            CartItem::where('user_id', Auth::id())->delete();
+            return view('buy/complete');
+        }
+        $request->flash();
+        return $this->index();
+    }
+}
--- ecsite/routes/web.php
+++ ecsite/routes/web.php
@@ -22,3 +22,13 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'ItemController@index');
 
 Route::get('/item/{item}', 'ItemController@show');
+
+Route::post('/cartitem', 'CartItemController@store');
+
+Route::get('/cartitem', 'CartItemController@index');
+
+Route::delete('/cartitem/{cartItem}', 'CartItemController@destroy');
+Route::put('/cartitem/{cartItem}', 'CartItemController@update');
+
+Route::get('/buy', 'BuyController@index');
+Route::post('/buy', 'BuyController@store');
